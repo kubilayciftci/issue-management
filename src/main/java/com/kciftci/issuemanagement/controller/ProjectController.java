52cd3bcf45644d6a73a03b3203dec48a5dac8768
kubilayciftci <kubilayciftci@outlook.com>
2020-11-01 23:07:01 +0300

    swagger doc added.

--- src/main/java/com/kciftci/issuemanagement/controller/ProjectController.java
+++ src/main/java/com/kciftci/issuemanagement/controller/ProjectController.java
@@ -2,11 +2,16 @@ package com.kciftci.issuemanagement.controller;
 
 import com.kciftci.issuemanagement.dto.ProjectDto;
 import com.kciftci.issuemanagement.service.implementation.ProjectServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/project")
+@Api(value = "/project", description = "User APIs")
 public class ProjectController {
 
     private final ProjectServiceImpl projectServiceImpl;
@@ -16,6 +21,7 @@ public class ProjectController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "get by id operation", response = ProjectDto.class)
     public ResponseEntity<ProjectDto> getById(@PathVariable(value = "id") Long id) {
         ProjectDto projectDto = projectServiceImpl.getById(id);
         return ResponseEntity.ok(projectDto);
@@ -23,18 +29,21 @@ public class ProjectController {
 
     //TODO Solve @Valid annotation problem and add it.
     @PostMapping
+    @ApiOperation(value = "create project operation", response = ProjectDto.class)
     public ResponseEntity<ProjectDto> createProject(@RequestBody ProjectDto projectDto) {
         return ResponseEntity.ok(projectServiceImpl.save(projectDto));
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "update project operation", response = ProjectDto.class)
     public ResponseEntity<ProjectDto> updateProject(@PathVariable(value = "id") Long id, @RequestBody ProjectDto projectDto) {
         ProjectDto update = projectServiceImpl.update(id, projectDto);
         return ResponseEntity.ok(update);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable(value = "id") Long id){
+    @ApiOperation(value = "delete project operation", response = Boolean.class)
+    public ResponseEntity<Boolean> delete(@PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(projectServiceImpl.delete(id));
     }
 }
