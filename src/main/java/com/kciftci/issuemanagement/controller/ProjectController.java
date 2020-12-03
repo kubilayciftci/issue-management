99c54ddb1e46bf24e3026fb98d5f31031e57485d
kubilayciftci <kubilayciftci@outlook.com>
2020-12-03 18:47:21 +0300

    added log info and debug

--- src/main/java/com/kciftci/issuemanagement/controller/ProjectController.java
+++ src/main/java/com/kciftci/issuemanagement/controller/ProjectController.java
@@ -5,12 +5,14 @@ import com.kciftci.issuemanagement.service.implementation.ProjectServiceImpl;
 import com.kciftci.issuemanagement.util.ApiPaths;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(ApiPaths.ProjectPaths.ISSUE_PATH)
-@Api(value = ApiPaths.ProjectPaths.ISSUE_PATH, description = "User APIs")
+@RequestMapping(ApiPaths.ProjectPaths.PROJECT_PATH)
+@Api(value = ApiPaths.ProjectPaths.PROJECT_PATH, description = "User APIs")
+@Slf4j
 public class ProjectController {
 
     private final ProjectServiceImpl projectServiceImpl;
@@ -22,6 +24,8 @@ public class ProjectController {
     @GetMapping("/{id}")
     @ApiOperation(value = "get by id operation", response = ProjectDto.class)
     public ResponseEntity<ProjectDto> getById(@PathVariable(value = "id") Long id) {
+        log.info("Project Controller->GetById");
+        log.debug("Project Controller->GetByID->Param:" + id);
         ProjectDto projectDto = projectServiceImpl.getById(id);
         return ResponseEntity.ok(projectDto);
     }
